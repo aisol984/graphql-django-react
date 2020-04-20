62b45fede5fa10a2a8edccc2a060cca83db6d629
westlepor <westlepor984@gmail.com>
2020-04-20 23:53:23 +0200

    add model

--- projectmanagement/project/models.py
+++ projectmanagement/project/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=10)
+    user_birthday = models.DateField()
+
+    def __str__(self):
+        return self.user_name
+
+class Project(models.Model):
+    user_id = models.ForeignKey(User,\
+        related_name='projects', on_delete=models.CASCADE)
+    project_name = models.CharField(max_length=10)
+    total_budget = models.IntegerField()
+    deadline = models.DateField()
+
+    def __str__(self):
+        return self.project_name
+
+class Income(models.Model):
+    project_id = models.ForeignKey(Project,\
+        related_name='income', on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, \
+        related_name='income', on_delete=models.CASCADE)
+    date = models.DateField()
